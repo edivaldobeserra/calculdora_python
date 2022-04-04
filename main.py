ecac1ad2d5a86875c55ca1271838a256b5a6605b
edivaldobeserra <97808328+edivaldobeserra@users.noreply.github.com>
2022-04-04 20:04:43 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import*
+from tkinter import ttk 
+
+
+
+cor1= "#080807"  #Preto
+cor2= "#ffffff"  #Branco
+cor3= "#0d098f"  #Azul
+cor4= "#6b6b70"  #cinza
+cor5= "#eb884b"  #Laranja
+
+
+
+janela=Tk() #criar a janela
+janela.title("calculadora") #titulo da janela
+janela.geometry("235x318") #tamanho da janela
+janela.mainloop() 
