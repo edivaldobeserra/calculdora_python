de8148318dfd3dfee5f1d4e10256c68a48c53891
edivaldobeserra <97808328+edivaldobeserra@users.noreply.github.com>
2022-04-05 19:15:23 -0300

    continuacao do projeto

--- main.py
+++ main.py
@@ -17,5 +17,24 @@ janela=Tk() #criar a janela
 janela.title("calculadora") #titulo da janela
 janela.geometry("235x318") #tamanho da janela
 janela.config(bg=cor1) #mudando cor da janela
+
+#criando Frames
+Frame_tela=Frame(janela, width=235, height=50, bg=cor3)
+Frame_tela.grid(row=0,column=0) 
+
+Frame_corpo=Frame(janela, width=235, height=268)
+Frame_corpo.grid(row=1,column=0) 
+
+#criando botoes
+
+b_1 = Button(Frame_corpo, text="C", width=15, height=2, bg=cor4, font=('iv 9 bold'), relief=RAISED, overrelief=RIDGE)
+b_1.place(x=0, y=0)
+b_2 = Button(Frame_corpo, text="%", width=7, height=2, bg=cor4, font=('iv 9 bold'), relief=RAISED, overrelief=RIDGE)
+b_2.place(x=117, y=0)
+b_3 = Button(Frame_corpo, text="/", width=7, height=2, bg=cor5, fg=cor2, font=('iv 9 bold'), relief=RAISED, overrelief=RIDGE)
+b_3.place(x=178, y=0)
+
+
 janela.mainloop() 
+
 
